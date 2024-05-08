929ab7ae0a332386ae32cc2529350cd65441955b
Eventtime2024 <168448599+Eventtime2024@users.noreply.github.com>
2024-05-08 22:17:09 +0300

    Update lilit.js

--- js/lilit.js
+++ js/lilit.js
@@ -1,19 +1,41 @@
 var viewportWidth = window.innerWidth;
 
-$(window).on( 'load', function() {
-    $('html, body').animate({scrollTop : 0});
-   
+$(window).on('load', function () {
+    $('html, body').animate({ scrollTop: 0 });
+
 })
-// $(document).on("contextmenu", function(e) {
-//     e.preventDefault();
-// });
+$(document).on("contextmenu", function (e) {
+    e.preventDefault();
+});
 
 
-function SendMail(){
-   var parms={
-       choose:document.getElementsByName('choose').value,
-       guestname:document.getElementById("#guestname").value,
-       guestname:document.getElementById("#guestnumber").value
-   }
-    emailjs.send("service_p65aldn","template_jzfinoc",parms)
+function sendMail(e) {
+
+    let params = {
+        choose: document.querySelector('input[name="choose"]:checked').value,
+        guestname: document.getElementById("guestname").value,
+        guestnumber: document.getElementById("guestnumber").value,
+    };
+
+    const serviceID = "service_4kvupjb";
+    const templateID = "template_17qflhh";
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            (document.querySelector('input[name="choose"]:checked').value = ""),
+                (document.getElementById("guestname").value = ""),
+                (document.getElementById("guestnumber").value = ""),
+                console.log(res);
+            console.log("Succesfully");
+            window.location.reload();
+        })
+        .catch((err) => console.log(err));
 }
+
+const sendMailbtn = document.querySelector("#sendMail");
+
+sendMailbtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    sendMail();
+   alert("сообщение отправлено");
+});
